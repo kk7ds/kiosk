3fdc68f12248186fe39331b134048ce75d2158c0
Dan Smith <dsmith@danplanet.com>
2024-08-20 12:36:10 -0700

    Stash ansible in a payload partition

--- installer/mkiso.sh
+++ installer/mkiso.sh
@@ -19,11 +19,18 @@ mkdir "$TMP"
 
 cp "${SRCDIR}/${PRESEED}" preseed.cfg
 xorriso -osirrox on -indev "$SRCISO" -extract / "$TMP"
-chmod +w -R "${TMP}/install.amd" "${TMP}/md5sum.txt"
+chmod +w -R "${TMP}/install.amd" "${TMP}/md5sum.txt" "${TMP}/boot/grub"
 gunzip "${TMP}/install.amd/initrd.gz"
 echo preseed.cfg | cpio -H newc -o -A -F "${TMP}/install.amd/initrd"
 gzip "${TMP}/install.amd/initrd"
 (cd "$TMP"; find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt)
+
+dd if=/dev/zero of=payload.vfat bs=1M count=256
+mkfs.vfat -n kiosk payload.vfat
+mcopy -s -i payload.vfat ${SRCDIR}/../{playbook.yaml,roles,runlocal.sh} ::
+
+# Remove graphical install boot target
+sed -ie '/Graphical/,+4d' ${TMP}/boot/grub/grub.cfg
 
 xorriso -as mkisofs \
    -r \
@@ -36,4 +43,5 @@ xorriso -as mkisofs \
    -e boot/grub/efi.img \
    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
    -no-emul-boot -isohybrid-gpt-basdat \
+   -append_partition 3 0x0c payload.vfat \
    "$TMP"
