184404562d9dd8ecfac4c9f2a6f0fe1edbd81507
Dan Smith <dsmith@danplanet.com>
2024-08-20 12:42:58 -0700

    Fix runlocal

--- runlocal.sh
+++ runlocal.sh
@@ -6,4 +6,9 @@ elif [ -f "settings.yaml" ]; then
     extra="--extra-vars=@settings.yaml"
 fi
 
+if ! mount | grep -q dev.shm; then
+    mkdir -p /dev/shm
+    mount -t tmpfs none /dev/shm
+fi
+
 ansible-playbook --connection=local -i $(hostname -s), $extra playbook.yaml -D
